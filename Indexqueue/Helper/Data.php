e54d4c11e6c3280c18857104fa9e3088aa2a72a6
Roberto Gambuzzi <gbinside@gmail.com>
2014-02-02 14:08:48 +0000

    first commit2

---
+++ Indexqueue/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Roberto
+ * Date: 02/02/14
+ * Time: 15.06
+ */ 
+class Gbinside_Indexqueue_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
---
+++ Indexqueue/sql/gbindexq_setup/mysql4-install-0.1.0.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Roberto
+ * Date: 02/02/14
+ * Time: 15.06
+ */ 
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+
+
+$installer->endSetup();
